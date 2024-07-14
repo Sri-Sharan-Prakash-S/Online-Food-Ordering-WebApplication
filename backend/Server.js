1572b056801081d52103825bda23a4bb9245349c
Sri-Sharan-Prakash-S <srisharanprakashsuriya@gmail.com>
2024-07-14 17:18:16 +0530

    update 4

--- backend/Server.js
+++ backend/Server.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/UserRoute.js"
+import 'dotenv/config'
 
 const app=express()
 const port=4000
@@ -14,6 +16,7 @@ connectDB();
 
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
---
+++ backend/controllers/UserController.js
@@ -0,0 +1,45 @@
+import userModel from "../models/UserModel.js";
+import jwt from "jsonwebtoken"
+import bcrypt from 'bcrypt'
+import validator from 'validator'
+
+const loginUser=async(req,res)=>{
+}
+
+
+const createToken=(id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
+
+const registerUser=async(req,res)=>{
+    const {name,password,email}=req.body;
+    try{
+        const exists=await userModel.findOne({email});
+        if(exists){
+            return res.json({success:false,message:"User Already Exists"});
+        }
+        if(!validator.isEmail(email)){
+            return res.json({success:false,message:"Please enter a valid email"})
+        }
+        if(password.length<8){
+            return res.json({success:false,message:"Please enter a strong password"})
+        }
+        const salt=await bcrypt.genSalt(10)
+        const hashedPassword=await bcrypt.hash(password,salt);
+        const newUser=new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword,
+        });
+
+        const user=await newUser.save()
+        const token=createToken(user._id)
+        res.jason({success:true,token});
+    }
+    catch(error){
+        console.log(error)
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export {loginUser,registerUser}
